0e1d14977f70fec85c2a18d59eb10cc9f7a13ccb
Huk Anna <gukanna606@gmail.com>
2020-11-01 16:35:35 +0300

    firstcom

---
+++ src/bsu/rfect/java/lab2/MainFrame.java
@@ -0,0 +1,16 @@
+package bsu.rfect.java.lab2;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.*;
+import static java.lang.Math.*;
+
+@SuppressWarnings("serial")
+
+public class MainFrame extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
